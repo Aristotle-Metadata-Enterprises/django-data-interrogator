03a27ca367afc75169802f91be2a844dedede301
tobybellwood <theodore.therone@gmail.com>
2016-02-24 04:53:45 +0000

    add more

--- data_interrogator/settings.py
+++ data_interrogator/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'data_interrogator',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
