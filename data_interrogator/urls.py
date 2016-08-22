fb7a3a315cb9bb3b2735274b64a23b71ac875be8
Samuel Spencer <theodore.therone@gmail.com>
2016-08-22 01:09:02 +0000

    fix dj1.10 url

--- data_interrogator/urls.py
+++ data_interrogator/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'data_interrogator'
 urlpatterns = [
-    url(r'^room/$', 'data_interrogator.views.custom_table', name='custom_table'),
+    url(r'^room/$', views.custom_table, name='custom_table'),
     url(r'^column_generator/$', views.column_generator, name='column_generator'),
     url(r'^table(?P<url>/.*/)?$', views.datatable, name='datatablepage'),
     url(r'^pivot/$', views.pivot.pivot_table, name='pivot'),
