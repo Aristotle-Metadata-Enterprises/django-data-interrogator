756631d80bb90f544d00b0920432369e7c0ac07c
Samuel Spencer <theodore.therone@gmail.com>
2016-08-23 01:40:03 +0000

    py3?

--- data_interrogator/admin.py
+++ data_interrogator/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.forms.models import BaseInlineFormSet
 from django.utils.translation import ugettext_lazy as _
-import models, forms
+from . import models, forms
 from django.conf import settings
 
 class TabularInlineWithRequestInFormset(admin.TabularInline):
