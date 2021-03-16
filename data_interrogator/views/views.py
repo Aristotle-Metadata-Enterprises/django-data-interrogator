1f7fd1a63941ac303a7ec5917405b18c7405f8c6
Dylan Silke <d.ylan.s.silke@gmail.com>
2021-03-17 10:31:21 +1100

    Chore: fixes bug in user_has_property permission check code

--- data_interrogator/views/views.py
+++ data_interrogator/views/views.py
@@ -54,7 +54,7 @@ class UserHasPermissionMixin(UserPassesTestMixin):
     def user_has_property(self) -> bool:
         """Returns if user has a particular property"""
         if user_permission := getattr(settings, 'INTERROGATOR_USER_PERMISSION_NAME', None):
-            return self.request.user.is_authenticated and user_permission
+            return self.request.user.is_authenticated and getattr(self.request.user, user_permission, None)
 
         return False
 
