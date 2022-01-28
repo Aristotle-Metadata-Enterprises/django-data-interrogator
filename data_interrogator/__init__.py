0b2ccafd80795026ce71f69634d8b29481249a50
Sam Spencer <theodore.therone@gmail.com>
2022-01-28 07:45:24 -0600

    correct version number

--- data_interrogator/__init__.py
+++ data_interrogator/__init__.py
@@ -2,10 +2,10 @@ default_app_config = 'data_interrogator.apps.InterrogatorConfig'
 
 __version_info__ = {
     'major': 0,
-    'minor': 0,
-    'micro': 2,
+    'minor': 1,
+    'micro': 0,
     'releaselevel': 'beta',
-    'serial': 2
+    'serial': 1
 }
 
 
