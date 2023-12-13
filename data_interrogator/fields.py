0c1be4b5bff881b501ea59aa4be8c6e38d45d5f2
Dylan <dylan.silke@proton.me>
2023-12-13 11:41:01 +1100

    Adds configurable delimiter for CSVMultipleCharField

--- data_interrogator/fields.py
+++ data_interrogator/fields.py
@@ -2,9 +2,18 @@ from django import forms
 
 
 class CSVMultipleCharField(forms.CharField):
+    """
+    CSV field with configurable delimiter.
+    Pipe delimiter is default for historical reasons, as previously it was the only delimiter possible.
+    """
     widget = forms.Textarea
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if delimiter := kwargs['delimiter']:
+            self.delimiter = delimiter
+        else:
+            self.delimiter = "|"
+
         self.widget.attrs = {'class': 'multicharfields'}
 
     def compress(self, values):
@@ -16,4 +25,4 @@ class CSVMultipleCharField(forms.CharField):
     def clean(self, value):
         if not value:
             value = ""
-        return [val for v in value.split("|") if (val := v.strip())]
+        return [val for v in value.split(self.delimiter) if (val := v.strip())]
